0ae7a22f36b0141f7831186bdd0194df6272dfbd
Xavier G Medina <130258237+xavimedi@users.noreply.github.com>
2023-09-21 12:19:00 -0500

    COB Sep 21

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,20 @@
 function ListGroup() {
-  const items = ["New York"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
   return (
     <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={(event) => console.log(event)}
+          >
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
