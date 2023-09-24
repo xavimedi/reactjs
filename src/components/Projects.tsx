e9dcca9b5fa2dec8bf1f1ce38109f4af9c5df926
Xavier G Medina <130258237+xavimedi@users.noreply.github.com>
2023-09-23 22:18:16 -0500

    finished 3rd project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,24 +4,35 @@ const projectsData = [
   {
     title: "KXZ Residentials",
     photo: "Residentials.png",
-    link: "https://java.codeup.com/capstone/",
     github: "https://github.com/Kyaw-Hughes-Medina-Capstone/Residential",
-    description: "Description for Project 1",
+    description:
+      "A full stack web application for bringing together property managers" +
+      " with potential renters while enhancing the documentation capabilities" +
+      " for repairs, work orders of properties, and inquiries for potential" +
+      " rental properties. Listings can be made with a full comprehensive" +
+      " list of features and photos. Utilizes MapBox and FileStack for geolocation" +
+      " and image hosting, respectively.",
     langs: "Java, HTML, CSS, Spring, MapBox, MySQL",
   },
   {
     title: "CoffeeScript Café",
     photo: "CoffeeScript.png",
-    link: "https://java.codeup.com/javascript-i/coffee-project/",
     github: "https://github.com/Medina-OSullivan-Coffee-Project/coffee-project",
-    description: "Description for Project 2",
+    description:
+      "A full stack web application for searching, sorting, and filtering" +
+      " a collection of coffees with various roasts and flavors. Allows a user" +
+      " to add coffee by name and roast by allowing the creation of a JavaScript object.",
     langs: "JavaScript, HTML, CSS, jQuery, Unit Testing",
   },
   {
-    title: "Project 3",
-    photo: "project3.jpg",
-    link: "https://example.com/project2",
-    description: "Description for Project 3",
+    title: "Java Adlister",
+    photo: "Adlister.png",
+    github: "https://github.com/Hughes-Mata-Medina-Adlister/Adlister",
+    description:
+      "A full stack web application designed to be a 'Craigslist' clone" +
+      " that allows the creation of a user account, required to post, edit, and" +
+      " view other ads posted. User information in the database is encrypted with JBCrypt.",
+    langs: "JavaScript, JSP, HTML, CSS, TomCat, JSP, MySQL, JDBC, JBCrypt",
   },
 ];
 
@@ -35,9 +46,6 @@ class ProjectCard extends React.Component<{ project: any }> {
           {project.title}
           <p>{project.description}</p>
           <p>{project.langs}</p>
-          <a href={project.link} target="_blank">
-            learn more
-          </a>
           <a href={project.github} target="_blank">
             github
           </a>
