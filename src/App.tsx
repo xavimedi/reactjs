2836d99540eb9fcbf39b1b0607867f5461aa872e
Xavier G Medina <130258237+xavimedi@users.noreply.github.com>
2023-09-26 10:08:21 -0500

    adding links to bio page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Biography from "./components/Biography";
-import Navigation from "./components/Navigation";
-import Background from "./components/Background";
 import Projects from "./components/Projects";
+import Background from "./components/Background";
+import Navigation from "./components/Navigation";
 
 function App() {
+  // Create state variables to manage section visibility
+  const [showBiography, setShowBiography] = useState(true);
+  const [showProjects, setShowProjects] = useState(false);
+
+  // Function to toggle section visibility
+  const toggleSection = ({ section }: { section: any }) => {
+    setShowBiography(section === "biography");
+    setShowProjects(section === "projects");
+  };
+
   return (
-    <>
+    <div>
       <Background />
       <Navigation />
-      <Biography />
-      <Projects />
-    </>
+      <div className="buttons">
+        <a href="#" onClick={() => toggleSection({ section: "biography" })}>
+          About
+        </a>
+        <a href="#" onClick={() => toggleSection({ section: "projects" })}>
+          Projects
+        </a>
+      </div>
+      {/* Conditional rendering of sections based on state */}
+      {showBiography && <Biography />}
+      {showProjects && <Projects />}
+    </div>
   );
 }
 
--- src/components/Biography.tsx
+++ src/components/Biography.tsx
@@ -1,7 +1,41 @@
-import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCss3,
+  faGithub,
+  faHtml5,
+  faJava,
+  faSquareJs,
+} from "@fortawesome/free-brands-svg-icons";
+
+let headshot = "/headshot.jpg";
+let bio = "bio";
+let github = "https://github.com/xavimedi";
+let blank = "_blank";
 
 function Biography() {
-  return <div></div>;
+  return (
+    <>
+      <div className={bio}>
+        <div>
+          <img src={headshot} />
+          <FontAwesomeIcon icon={faJava} />
+          <FontAwesomeIcon icon={faSquareJs} />
+          <FontAwesomeIcon icon={faCss3} />
+          <FontAwesomeIcon icon={faHtml5} />
+
+          <a href={github} target={blank}>
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
+        <div>12341234</div>
+      </div>
+    </>
+  );
 }
 
 export default Biography;
